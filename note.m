f88cd54cd1f0d01c9a3d0032715b229ff568fc25
Trfaxa <oznurr_yilmazz@hotmail.com>
2015-12-15 12:32:46 +0200

    frekansına ve vuruşlarına göre notaların sinus sinyallerini çeviren fonksiyon yazıldı

--- note.m
+++ note.m
@@ -1,3 +1,3 @@
-function [x,t]=note (F,dur)
-t=0:1/1000000; dur;
-x=sin(2*pi*f*t);
+function [x,t]=note (F,dur) 
+t=0:1/1000:dur;
+x=sin(2*pi*F*t);%girilen bilgilerin çýkýþlarýný sinüs sinyali döndürüldü.
