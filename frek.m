ac0c6e91525059782829f92f6354122618b7093c
Trfaxa <oznurr_yilmazz@hotmail.com>
2015-12-15 13:09:55 +0200

    yuvarlama yapılmadan frekans hesaplaması yapıldı

--- frek.m
+++ frek.m
@@ -2,4 +2,4 @@ function [F]=frek(n,o)%frekans döndürmektedir.
 nota={'do','c#','re','eb','mi','fa','f#','sol','g#','la','bb','si'}; %notolardan oluþan bir dizi yazdýk.
 n=strcmp(nota,n); %Dizenin içine aktarý,indekside 1 artýrýr.
 m=find(n,1);%n dizisinde 1 deðerinin indeksini döndürür.
-F=round(16.35*(2^o)*(2^((m-1)/12)));%frekans fonksiyonu oluþturuldu.
+F=16.35*(2^o)*(2^((m-1)/12));%frekans fonksiyonu oluþturuldu.
