2ef5adcb2105d401f95e21ca4862828fdc875e79
Trfaxa <oznurr_yilmazz@hotmail.com>
2015-12-22 12:35:00 +0200

    note.m dosyası duzenlendi

--- note.m
+++ note.m
@@ -1,3 +1,20 @@
-function [x,t]=note (F,dur) 
-t=0:1/1000:dur;
-x=sin(2*pi*F*t);%girilen bilgilerin çýkýþlarýný sinüs sinyali döndürüldü.
+function [x,t] = note(f,vurus) %fonksiyonu tanimliyoruz.girilicek argumanlari tanýmladýk
+fs=8192;%örnekleme frekansý
+t = 0:1/fs:vurus; %0 dan dur a kadar 1/(100*ff) kadar artýyor
+%xx,tt]=note(1,3);
+%plot(tt,xx)
+%grafigi cizdirmek icin ustteki kodu command window'a yazariz
+
+    elemanS = length(tt); %% Eleman sayýsý
+    
+    attack = linspace(0,1.5,(elemanS*2/8));
+    
+    decay = linspace(1.5-(0.5/(elemanS/8)),1,(elemanS/8));
+    
+    sustain = ones(1,(elemanS/2));
+    
+    relase = linspace(1-(1/(elemanS/8)),0,(elemanS/8));
+    
+    zarf = [attack, decay, sustain, relase];
+
+    x = zarf .* sin(2*pi*f*t + zarf); %% zarflý sinüs
