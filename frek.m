e21972318090a032ae04c97a064b65fffcbbaa17
Trfaxa <oznurr_yilmazz@hotmail.com>
2015-12-22 12:34:17 +0200

    frek.m duzenlendi

--- frek.m
+++ frek.m
@@ -1,5 +1,24 @@
-function [F]=frek(n,o)%frekans döndürmektedir.
-nota={'do','c#','re','eb','mi','fa','f#','sol','g#','la','bb','si'}; %notolardan oluþan bir dizi yazdýk.
-n=strcmp(nota,n); %Dizenin içine aktarý,indekside 1 artýrýr.
+function [frekans]=frek(nota,oktav) %Girilen nota ve oktava göre frekans döndürülüyor.
+notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'}; %Notalarýmýz bir dizi içerisinde yazýldý.
+n=strcmp(notalar,nota); %Fonksiyona gönderdiðimiz notayý dizi içerisinde aratýr ve buduðu indekse 1 atar
 m=find(n,1);%n dizisinde 1 deðerinin indeksini döndürür.
-F=16.35*(2^o)*(2^((m-1)/12));%frekans fonksiyonu oluþturuldu.
+
+if (nargin==0)
+    
+    disp ('Lütfen veri giriþi yapýnýz: ')
+    
+else if (nargin==1)
+        if(m==13)
+            frekans=0;
+        else
+           frekans=floor(16.35*(2^4)*(2^((m-1)/12))); % Frekans fonksiyonu oluþturuldu. 
+           
+        end
+    else 
+        if (m==13) 
+            frekans=0;
+        else frekans=floor(16.35*(2^oktav)*(2^((m-1)/12))); % Frekans fonksiyonu oluþturuldu.
+        end
+        
+    end
+end
